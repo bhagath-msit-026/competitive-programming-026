2be4a1b439aabbac914668c2a6a9f6a6261ec284
bhagath-msit-026 <72123242+bhagath-msit-026@users.noreply.github.com>
2021-07-26 08:13:33 +0530

    bhagath_multiplypoynomials

--- multiply_polynomials.py
+++ multiply_polynomials.py
@@ -8,7 +8,22 @@
 
 def multiplyPolynomials(p1, p2):
     # Your code goes here...
+    len_a=len(p1)
+    len_b=len(p2)
+    result=[]
+    result_len=len_a+len_b-1
+    
+    for i in range(result_len):
+        result.append(0)
+        
+    for i in range(len_a):
+        for j in range(len_b):
+            result[i+j]+=p1[i]*p2[j]
+    return result
     pass
 
 # Write your own test cases
-print ("All test cases passwed...")
+print ("All test cases passed...")
+p1=[2,0,3]
+p2=[4,5]
+print(multiplyPolynomials(p1,p2))
