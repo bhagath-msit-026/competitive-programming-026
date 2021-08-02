2f7175ffd370049696cf16094c4b85d8fa216c9e
bhagath-msit-026 <72123242+bhagath-msit-026@users.noreply.github.com>
2021-08-02 08:46:11 +0530

    bhagath_nthcarolprime

--- 04-nth_carolprime-Python/nth_carolprime.py
+++ 04-nth_carolprime-Python/nth_carolprime.py
@@ -8,6 +8,21 @@
 # Hint: you may need to generate only Carol numbers, and then test those as you go 
 # for primality (and you may need to think about that hint for a while for it to make sense!).
 
+def isPrime(n):
+    if(n<2):
+        return False
+    for x in range(2,n):
+        if(n%x==0):
+            return False
+    return True
 
 def fun_nth_carolprime(n):
-    return 0
+    num=2
+    count=0
+    result=-1
+    while (count<=n):
+        result = ((2**num-1)**2-2)
+        if (isPrime(result)):
+            count = count+1
+        num = num+1
+    return result
